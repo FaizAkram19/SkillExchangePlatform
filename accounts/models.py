4227ec1435bfe34e74a07c702e380ca7d4ac2d20
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-27 21:17:31 +0000

    Created the models for User and Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    pass 
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    dp=models.URLField(blank=True)
+    timezone=models.CharField(max_length=25, default='Asia/Kolkata')
+    availability=models.JSONField(default=dict)
+    rating=models.DecimalField(max_digits=3, decimal_places=2, default=None, null=True)
