30534765cff0538f094fe56aa39ce797a6c6cc55
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-06 12:24:56 +0000

    Created the signal for auto-creating Profile instance

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ AUTH_USER_MODEL='accounts.User'
 
 INSTALLED_APPS = [
     'rest_framework',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
