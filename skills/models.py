6b4f423f1475406ce771d908fabdd43a36edea65
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-07 12:47:45 +0000

    Made the model that should handle general/new skills

--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    name=models.CharField(max_length=30, unique=True, blank=False)
+    description=models.TextField(blank=True)
+    is_approved=models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.name=self.name.lower()
+        super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.name
