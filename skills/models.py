5109b3880153d436bdc84ac3c27e796046726c36
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-07 13:26:33 +0000

    Created the UserSkill model The UserSkill is made in such a way that it can handle duplicates and it gives 2 choices for each skill.

--- skills/models.py
+++ skills/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 class Skill(models.Model):
@@ -12,3 +13,19 @@ class Skill(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class UserSkill(models.Model):
+    SKILL_CHOICE={
+        "s": "Seeking",
+        "o": "Offering",
+    }
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    skill=models.ForeignKey(Skill, on_delete=models.CASCADE)
+    skill_type=models.CharField(max_length=1, choices=SKILL_CHOICE)
+
+    def __str__(self):
+        return self.skill.name
+    
+    class Meta:
+        unique_together=[["user", "skill", "skill_type"]]
