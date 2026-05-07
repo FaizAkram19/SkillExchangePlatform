5483bcb281d1b2b39af0de7d9fc4d81c30afa3a1
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-07 18:41:50 +0000

    ConnectionRequest was set up This model defines how two users would interact, when they would want to connect with each other.

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+class ConnectionRequest(models.Model):
+    status_type={
+        "a": "Approved",
+        "r": "Rejected",
+        "p":"Pending"
+    }
+    sender=models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_requests")
+    receiver=models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_requests")
+    connectionStatus=models.CharField(max_length=1, choices=status_type, default="p")
+
+    class Meta:
+        unique_together=[["sender", "receiver"]]
