d66562ed443bb148c2cf1ef617a91547290e3658
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-13 15:26:01 +0000

    Setup Cross-Origin Resource Sharing for the backend

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,7 @@ AUTH_USER_MODEL='accounts.User'
 
 INSTALLED_APPS = [
     'rest_framework',
+    'corsheaders',
     'accounts.apps.AccountsConfig',
     'skills.apps.SkillsConfig',
     'connections.apps.ConnectionsConfig',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
 ]
 
 ROOT_URLCONF = 'core.urls'
