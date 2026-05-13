a32590d600cb178fcd3ad00ab95da5366889ec72
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-13 16:03:17 +0000

    Made changes to UserSerializer to handle user registration in frontend

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,10 @@ from accounts.models import User, Profile
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=["username", "email", "first_name", "last_name"]
+        fields=["username", "email", "first_name", "last_name", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
 
 class ProfileSerializer(serializers.ModelSerializer):
     user=UserSerializer()
