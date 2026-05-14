3b58bbe97296e461dbb17322f873d84c9acc9abc
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-05-14 18:19:34 +0000

    Made changes to the ProfileSerializer made first_name and last_name Profile fields so that they can be edited by a user. Can't just give them the User model you know

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,10 +10,26 @@ class UserSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user=UserSerializer()
+    # first_name and last_name are declared as explicit write_only fields on ProfileSerializer
+    # because they belong to the User model, not Profile. Since user is read_only (to prevent
+    # the client from changing which user owns the profile), we can't write to User fields
+    # through the nested UserSerializer. Instead we accept them as top-level fields and
+    # manually update instance.user in the overridden update() method.
+    user=UserSerializer(read_only=True)
+    first_name = serializers.CharField(write_only=True, required=False)
+    last_name = serializers.CharField(write_only=True, required=False)
     class Meta:
         model=Profile
-        fields=["user", "dp", "timezone", "availability", "rating"]
+        fields=["user","first_name", "last_name", "dp", "timezone", "availability", "rating"]
         extra_kwargs={
             "rating":{'read_only':True}
         }
+    def update(self, instance, validated_data):
+        instance.user.first_name = validated_data.get("first_name", instance.user.first_name)
+        instance.user.last_name = validated_data.get("last_name", instance.user.last_name)
+        instance.user.save()
+
+        instance.timezone=validated_data.get("timezone", instance.timezone)
+        instance.availability=validated_data.get("availability", instance.availability)
+        instance.save()
+        return instance
