f6a7069486a482975103fa0399e46251ca6308b3
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-08-05 16:08:10 +0000

    Registered User and ConnectionRequest on the admin site

--- connections/admin.py
+++ connections/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ConnectionRequest
 
-# Register your models here.
+
+@admin.register(ConnectionRequest)
+class ConnectionRequestAdmin(admin.ModelAdmin):
+    list_display = ("sender", "receiver", "connectionStatus")
+    list_filter = ("connectionStatus",)
+    search_fields = ("sender__username", "receiver__username")
